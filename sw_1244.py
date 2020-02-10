a0a035663659603f78347df1cdb7359c01b6c797
이연재 <duseh73@gmail.com>
2020-02-10 19:48:52 +0900

    20_02_10

---
+++ sw_1244.py
@@ -0,0 +1,6 @@
+import sys
+sys.stdin = open('./input/input_1244.txt','r')
+
+import random
+ls = ['진웅','연재']
+print(random.choice(ls))
--- sw_4012.py
+++ sw_4012.py
@@ -9,3 +9,25 @@ for tc in range(TC):
         line = list(map(int,input().split()))
         f_map.append(line)
 
+    c = [x for x in range(0,N)]
+    n = len(c)
+    c_ls = []
+    for i in range(1 << n):
+        line = []
+        for j in range(n):
+            tf = i & (1 << j)
+            if tf:
+                line.append(c[j])
+        if len(line) == N//2:
+            c_ls.append(line)
+    
+    for food1 in c_ls:
+        for food2 in c_ls:
+            if sorted(food1+food2) != c:
+                continue
+            for i in food1:
+                for j in food1:
+                    if i == j:
+                        continue
+                    else:
+                        f_map[i][j]

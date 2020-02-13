01b7a961936db309aa56102ec4563eb3f73780da
patomatoo <duseh73@gmail.com>
2020-02-13 23:35:34 +0900

    20_02_13

--- SW_expert/sw_4869.py
+++ SW_expert/sw_4869.py
@@ -1,27 +1,8 @@
 TC = int(input())
-
-def recursive(n):
-    if n == N // 10:
-        return
-    else:
-        if arr[i] + arr[j] + arr[k] == N:
-            if arr[j] != 0:
-                result.append((arr[i], arr[j], arr[k]))
-        recursive(n+1)
-
-
-
-
 for tc in range(TC):
     N = int(input())
-    arr = [10, 20, 20, 0]
-    result = []
-    for i in range(len(arr)):
-        for j in range(len(arr)):
-            for k in range(len(arr)):
-                if arr[i] + arr[j] + arr[k] == N:
-                    if arr[j] != 0:
-                        result.append((arr[i], arr[j], arr[k]))
-    recursive(0)
-    result = set(result)
-    print(result)
+    a = 1
+    b = 3
+    for _ in range(N//10-2):
+        a, b = b, a*2+b
+    print('#{} {}'.format(tc+1,b))
--- SW_expert/sw_4873.py
+++ SW_expert/sw_4873.py
@@ -2,4 +2,20 @@ import sys
 sys.stdin = open('./input/input_4873.txt','r')
 
 TC = int(input())
+for tc in range(TC):
+    word = input()
+    words = []
+    for i in range(len(word)):
+        words.append(word[i])
 
+    while True:
+        Sum_ = 0
+        for i in range(len(words)-1):
+            if words[i] == words[i+1]:
+                del words[i]
+                del words[i]
+                Sum_ = 1
+                break
+        if Sum_ == 0:
+            break
+    print('#{} {}'.format(tc+1,len(words)))

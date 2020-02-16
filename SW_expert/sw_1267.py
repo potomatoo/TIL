26af0f85148af5ba4a0d274eda7b26da6d76f687
patomatoo <duseh73@gmail.com>
2020-02-16 22:41:53 +0900

    20_02_16

--- SW_expert/sw_1267.py
+++ SW_expert/sw_1267.py
@@ -6,23 +6,33 @@ for tc in range(10):
     G = [[] for _ in range(V+1)]
     visit = [0] * (V+1)
     S = []
-    road = list(map(int,input().split()))
-    print(road)
-    for i in range(0,len(road)-1,2):
-        G[road[i]].append(road[i+1])
-    print(G)
-    v = 1
-    visit[v] = 1
-    S.append(v)
-    while S:
-        for w in G[v]:
-            if visit[w]: continue
-            S.append(v)
-            visit[w] = 1
-            v = w
-            break
+    roads = list(map(int,input().split()))
+    road = [[roads[i], roads[i+1]] for i in range(0,len(roads)-1,2)]
+    front = [road[i][0] for i in range(len(road))]
+    back = [road[i][1] for i in range(len(road))]
+
+    for i in range(len(road)):
+        if road[i][0] not in back:
+            road[0], road[i] = road[i], road[0]
+
+    for i in range(len(road)):
+        for j in range(len(road)):
+            if road[i][1] == road[j][0]:
+                road[i], road[j] = road[j], road[i]
+
+    result = []
+    for i in range(len(road)):
+        if road[i][0] not in result:
+            result.append(road[i][0])
+        if road[i][1] not in result:
+            result.append(road[i][1])
+    print('#{}'.format(tc+1),end=' ')
+    for i in range(len(result)):
+        if i == len(result)-1:
+            print(result[i])
         else:
-            v = S.pop()
+            print(result[i], end=' ')
+
         
 
 

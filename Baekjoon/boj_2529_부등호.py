139501c6797d9e051ddb9f5e86419e9049a44c5c
patomatoo <duseh73@gmail.com>
2020-02-26 18:30:28 +0900

    20_02_26

--- Baekjoon/boj_2529_부등호.py
+++ Baekjoon/boj_2529_부등호.py
@@ -2,29 +2,18 @@ def permutiation(k,num):
     global flag
     if flag == True:
         if k == m:
-            if cal[m-2] == '<':
-                if order[-2] < order[-1]:
-                    for i in range(len(order)):
-                        print(order[i],end='')
-                    flag = False
-
-            if cal[m-2] == '>':
-                if order[-2] > order[-1]:
-                    for i in range(len(order)):
-                        print(order[i], end='')
-                    flag = False
-
+            for i in range(len(order)):
+                print(order[i],end='')
+                flag = False
         else:
             for i in range(len(num)):
                 if visit[i] == 1:
                     continue
-                if len(order) > 1:
-                    if cal[k - 1] == '<':
-                        if order[k - 3] > order[k - 2]:
-                            continue
-                    if cal[k - 1] == '>':
-                        if order[k - 3] < order[k - 2]:
-                            continue
+                if len(order) > 0:
+                    if cal[len(order)-1] == '<' and order[-1] > num[i]:
+                        continue
+                    if cal[len(order)-1] == '>' and order[-1] < num[i]:
+                        continue
                 order.append(num[i])
                 visit[i] = 1
                 permutiation(k+1,num)
@@ -33,13 +22,14 @@ def permutiation(k,num):
 
 N = int(input())
 cal = input().split()
-flag = True
 m = N+1
 order = []
+flag = True
 num = [9,8,7,6,5,4,3,2,1,0]
-num2 = [0,1,2,3,4,5,6,7,8,9]
 visit = [0] * len(num)
 permutiation(0,num)
+num2 =[0,1,2,3,4,5,6,7,8,9]
+visit2 = [0] * len(num2)
 flag = True
 print()
 permutiation(0,num2)

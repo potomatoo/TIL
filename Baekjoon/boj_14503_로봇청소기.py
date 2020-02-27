d5f60c71b988b3a2eaa5bdc6d9317b1619e27e32
patomatoo <duseh73@gmail.com>
2020-02-28 02:19:40 +0900

    20_02_28

--- Baekjoon/boj_14503_로봇청소기.py
+++ Baekjoon/boj_14503_로봇청소기.py
@@ -1,21 +1,31 @@
+n, m = map(int, input().split())
+x, y, d = map(int, input().split())
+c_map = [list(map(int, input().split())) for _ in range(n)]
+c_map[x][y] = 2
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+def solve(x, y, d, cnt):
+    while True:
+        c = False
+        for k in range(4):
+            nd = (d+3) % 4
+            tx = x + dx[nd]
+            ty = y+dy[nd]
+            d = nd
+            if c_map[tx][ty] == 0:
+                c_map[tx][ty] = 2
+                cnt += 1
+                x = tx
+                y = ty
+                c = True
+                break
+        if c == False:
+            if c_map[x-dx[d]][y-dy[d]] == 1:
+                return cnt
+            else:
+                x = x - dx[d]
+                y = y - dy[d]
 
-N, M = map(int,input().split())
-y, x, d = map(int,input().split())
-y = y-1
-x = x-1
-c_map = []
-for _ in range(N):
-    c_map.append(list(map(int,input().split())))
-visit = [[0 for _ in range(M)] for _ in range(N)]
-if d == 0:
-    dy = [0, -1, -1]
-    dx = [-1, -1, 0]
-if d == 1:
-    dy = [-1, -1, 0]
-    dx = [0, -1, -1]
-if d == 2:
-    dy = [0, -1, -1]
-    dx = [1, 1, 0]
-if d == 3:
-    dy = [1, 1, 0]
-    dx = [0, 1, 1]
+print(solve(x, y, d, 1))
+
+

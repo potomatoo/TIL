3ccc2dd8e7b4d1d38d062ed4773ac14cfbcc6adf
patomatoo <duseh73@gmail.com>
2020-03-02 02:15:07 +0900

    20_03_02

--- Baekjoon/boj_14502_연구소.py
+++ Baekjoon/boj_14502_연구소.py
@@ -1,5 +1,7 @@
 def dfs(y,x,cnt):
+    global width
     visit[y][x] = 1
+    width += 1
     for i in range(4):
         ty = y + dy[i]
         tx = x + dx[i]
@@ -13,15 +15,32 @@ i_map = []
 for _ in range(N):
     i_map.append(list(map(int,input().split())))
 visit = [[0 for _ in range(M)]for _ in range(N)]
+visit2 = [[0 for _ in range(M)]for _ in range(N)]
 dy = [-1, 1, 0, 0]
 dx = [0, 0, -1, 1]
 cnt = 0
+width = 0
+ans = []
+k = 0
 for y in range(N):
     for x in range(M):
-        if i_map[y][x] == 2 and visit[y][x] == 0:
-            cnt += 1
-            dfs(y,x,cnt)
-print(cnt)
-for y in range(N):
-    print(visit[y])
+        if k == 3:
+            for y in range(N):
+                for x in range(M):
+                    if i_map[y][x] == 2 and visit[y][x] == 0:
+                        cnt += 1
+                        dfs(y, x, cnt)
+                        ans.append(width)
+                        visit[y][x] = 0
+                        k = 0
+
+        elif i_map[y][x] == 0:
+            i_map[y][x] = 1
+            visit2[y][x] = 1
+            k += 1
+
+
+
+
+
 

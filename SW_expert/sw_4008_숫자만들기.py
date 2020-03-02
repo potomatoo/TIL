dd3c8a913c1fd4eae98b579818db4c8fdc110094
patomatoo <duseh73@gmail.com>
2020-03-02 23:52:53 +0900

    20_03_02

--- SW_expert/sw_4008_숫자만들기.py
+++ SW_expert/sw_4008_숫자만들기.py
@@ -1,82 +1,30 @@
 import sys
 sys.stdin = open('./input/input_4008.txt','r')
 
-def max_cal(k):
-    global ans
-    global max_ans
-    if max_ans < ans:
-        return
-    if k == N:
-        ans = order[0]
-        for i in range(1, len(order)):
-            if cal[i-1] == '+':
-                ans += order[i]
-            elif cal[i-1] == '-':
-                ans -= order[i]
-            elif cal[i-1] == '*':
-                ans *= order[i]
-            elif cal[i-1] == '/':
-                ans //= order[i]
-        max_ans = max(ans, max_ans)
+def DFS(k, n, s, op1, op2, op3, op4):
+    global MAX, MIN
+    if k == n:
+        if s > MAX:
+            MAX = s
+        if s < MIN:
+            MIN = s
         return
     else:
-        for i in range(N):
-            if visit[i] == 1:
-                continue
-            visit[i] = 1
-            order.append(nums[i])
-            max_cal(k+1)
-            visit[i] = 0
-            order.pop()
+        if op1 > 0:
+            DFS(k+1, n, s+num[k], op1-1, op2, op3, op4)
+        if op2 > 0:
+            DFS(k+1, n, s-num[k], op1, op2-1, op3, op4)
+        if op3 > 0:
+            DFS(k+1, n, s*num[k], op1, op2, op3-1, op4)
+        if op4 > 0:
+            DFS(k+1, n, int(s/num[k]), op1, op2, op3, op4-1)
 
-def min_cal(k):
-    global ans
-    global min_ans
-
-    if min_ans > ans:
-        return
-
-    if k == N:
-        ans = order[0]
-        for i in range(1, len(order)):
-            if cal[i-1] == '+':
-                ans += order[i]
-            elif cal[i-1] == '-':
-                ans -= order[i]
-            elif cal[i-1] == '*':
-                ans *= order[i]
-            elif cal[i-1] == '/':
-                ans //= order[i]
-        max_ans = max(ans, max_ans)
-        return
-
-    else:
-        for i in range(N):
-            if visit[i] == 1:
-                continue
-            visit[i] = 1
-            order.append(nums[i])
-            permutation(k+1)
-            visit[i] = 0
-            order.pop()
-
-TC = int(input())
-for tc in range(TC):
-    N = int(input())
-    d = ['+', '-', '*', '/']
-    k = list(map(int,input().split()))
-    cal = []
-    for i in range(len(d)):
-        if k[i] == 0:
-            continue
-        for _ in range(k[i]):
-            cal.append(d[i])
-    nums = list(map(int,input().split()))
-    visit = [0] * N
-    order = []
-    ans = 0
-    max_ans = 0
-    max_cal(0)
-    anx = 0
-    min_ans = 0xffffffff
-    
+for tc in range(1, int(input())+1):
+    n = int(input())
+    op1, op2, op3, op4 = map(int, input().split())
+    num = list(map(int, input().split()))
+    MAX = -0xffffffff
+    MIN = 0xffffffff
+    SUM = num[0]
+    DFS(1, n, SUM, op1, op2, op3, op4)
+    print('#%d %d' %(tc, MAX-MIN))

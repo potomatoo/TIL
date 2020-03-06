975ba37538b80fba3966b1563d37af27612fb1c2
patomatoo <duseh73@gmail.com>
2020-03-06 19:24:40 +0900

    20_03_06

--- Baekjoon/BOJ_1260_DFS와BFS.py
+++ Baekjoon/BOJ_1260_DFS와BFS.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 V, E, start = map(int,input().split())
 G = [[] for _ in range(V+1)]
 visit = [0] * (V+1)
@@ -5,6 +7,9 @@ for _ in range(E):
     u, v = map(int,input().split())
     G[u].append(v)
     G[v].append(u)
+for i in range(1,len(G)):
+    G[i].sort()
+
 S = []
 v = start
 S.append(v)
@@ -20,3 +25,21 @@ while S:
         break
     else:
         v = S.pop()
+print()
+Q = deque()
+visit = [0] * (V+1)
+v = start
+Q.append(v)
+print(v,end=' ')
+visit[v] = 1
+while Q:
+    v = Q.popleft()
+    for w in G[v]:
+        if visit[w] == 1:
+            continue
+        Q.append(w)
+        visit[w] = 1
+        print(w,end=' ')
+        v = w
+
+

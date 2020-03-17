ce54510e1ffa6a812a4f8fdb47013064ec383292
yeonjae <duseh73@gmail.com>
2020-03-18 00:44:02 +0900

    20_03_18

--- Baekjoon/boj_1941_소문난칠공주.py
+++ Baekjoon/boj_1941_소문난칠공주.py
@@ -1,47 +1,33 @@
-def dfs(y,x,cnt):
+def dfs(k):
     global ans
-    if order.count('Y') >= 4:
-        return
-    if cnt == 6:
-        # print(order)
-        # print(seven_princess)
-        ans += 1
-        return
 
-    for i in range(4):
-        ty = y + dy[i]
-        tx = x + dx[i]
-        if ty < 0 or tx < 0 or ty > 4 or tx > 4:
-            continue
-        if visit[ty][tx] == 0:
-            visit[ty][tx] = 1
-            seven_princess.append(sit_nums[ty][tx])
-            order.append(sit[ty][tx])
-            dfs(ty,tx,cnt+1)
-            seven_princess.pop()
-            order.pop()
-            visit[ty][tx] = 0
+    if k == 7:
+        print(order)
+        print(force)
+    else:
+        for y in range(5):
+            for x in range(5):
+                if visit[y][x] == 1: continue
+                visit[y][x] = 1
+                order.append(sit[y][x])
+                force.append(school[y][x])
+                dfs(k+1)
+                visit[y][x] = 0
+                order.pop()
+                force.pop()
+
+school = []
+for y in range(5):
+    school.append(input())
 
 sit = []
-for y in range(5):
-    sit.append(input())
-
-sit_nums = []
 k = 1
 for _ in range(5):
-    sit_nums.append([x for x in range(k,k+5)])
+    sit.append([x for x in range(k, k+5)])
     k += 5
-
 dy = [-1, 1, 0, 0]
 dx = [0, 0, -1, 1]
-ans = 0
-for y in range(5):
-    for x in range(5):
-        visit = [[0 for _ in range(5)] for _ in range(5)]
-        seven_princess = [sit_nums[y][x]]
-        order = [sit[y][x]]
-        visit[y][x] = 1
-        dfs(y, x, 0)
-print(ans)
-
-
+visit = [[0 for _ in range(5)] for _ in range(5)]
+order = []
+force = []
+dfs(0)

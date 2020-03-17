c5b5afbe1a5d11d906147ed1c67328dba3eab76d
yeonjae <duseh73@gmail.com>
2020-03-17 19:52:25 +0900

    20_03_17

--- Baekjoon/boj_17244_아맞다우산.py
+++ Baekjoon/boj_17244_아맞다우산.py
@@ -1,4 +1,18 @@
 from collections import deque
+from copy import deepcopy
+def permutation(k):
+    if k == len(item_y_x):
+        c_order = deepcopy(order)
+        y_x.append(c_order + [(f_y, f_x)])
+    else:
+        for i in range(len(item_y_x)):
+            if visited[i] == 1: continue
+            visited[i] = 1
+            order.append(item_y_x[i])
+            permutation(k+1)
+            visited[i] = 0
+            order.pop()
+
 M, N = map(int,input().split())
 home = []
 for _ in range(N):
@@ -6,43 +20,48 @@ for _ in range(N):
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
-ans = 0
-item = 0
+
 s_y, s_x = 0, 0
+item_y_x = []
 for y in range(N):
     for x in range(M):
         if home[y][x] == 'S':
             s_y, s_x = y, x
         if home[y][x] == 'X':
-            item += 1
+            item_y_x.append((y,x))
+        if home[y][x] == 'E':
+            f_y, f_x = y, x
 
-cnt = item
-visit2 = [[0 for _ in range(M)] for _ in range(N)]
-for _ in range(item+1):
-    Q = deque()
-    visit = [[0 for _ in range(M)] for _ in range(N)]
-    D = [[0 for _ in range(M)] for _ in range(N)]
-    Q.append((s_y, s_x))
-    visit[s_y][s_x] = 1
-    while Q:
-        yy, xx = Q.popleft()
-        if home[yy][xx] == 'X' and visit2[yy][xx] == 0:
-            visit2[yy][xx] = 1
-            ans += D[yy][xx]
-            s_y, s_x = yy, xx
-            cnt -= 1
-            break
-        if home[yy][xx] == 'E' and cnt == 0:
-            ans += D[yy][xx]
-            break
-        for i in range(4):
-            ty = yy + dy[i]
-            tx = xx + dx[i]
-            if ty < 0 or tx < 0 or ty > N-1 or tx > M-1:
-                continue
-            if home[ty][tx] != '#' and visit[ty][tx] == 0:
-                visit[ty][tx] = 1
-                Q.append((ty,tx))
-                D[ty][tx] = D[yy][xx] + 1
-print(ans)
+order = []
+visited = [0] * len(item_y_x)
+y_x = []
+permutation(0)
 
+min_time = 0xfffffff
+a, b = s_y, s_x
+for per in range(len(y_x)):
+    s_y, s_x = a, b
+    time = 0
+    for r, c in y_x[per]:
+        Q = deque()
+        Q.append((s_y, s_x, 0))
+        visit = [[0 for _ in range(M)] for _ in range(N)]
+        visit[s_y][s_x] = 1
+        while Q:
+            yy, xx, d = Q.popleft()
+            if yy == r and xx == c:
+                time += d
+                s_y, s_x = yy, xx
+            for i in range(4):
+                ty = yy + dy[i]
+                tx = xx + dx[i]
+                if ty < 0 or tx < 0 or ty > N-1 or tx > M-1:
+                    continue
+                if home[ty][tx] != '#' and visit[ty][tx] == 0:
+                    visit[ty][tx] = 1
+                    Q.append((ty, tx, d+1))
+
+    if time < min_time:
+        min_time = time
+
+print(min_time)

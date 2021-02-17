d14ca3189e106ccb75e86ce74d07f325354cb8f8
yeonjae <duseh73@gmail.com>
2021-02-17 17:29:28 +0900

    20210217

--- Programmers/LevelTest01(2).py
+++ Programmers/LevelTest01(2).py
@@ -1,13 +1,21 @@
-def solution(d, budget):
-    answer = 0
-    d.sort()
-    check = 0
-    for i in range(len(d)):
-        if check + d[i] > budget:
-            break
-        check += d[i]
-        answer += 1
+def solution(N, stages):
+    fail_stage = [0] * (N+2)
+
+    for i in range(1, N+2):
+        fail_stage[i] = stages.count(i)
+    for i in range(1, N+1):
+        arrive_stage = sum(fail_stage[i:])
+        if arrive_stage == 0:
+            fail_stage[i] = (i, 0)
+            continue
+        fail_stage[i] = (i, fail_stage[i] / arrive_stage)
+    fail_stage = fail_stage[1:-1]
+    fail_stage.sort(key=lambda x:[-x[1],x[0]])
+    answer = []
+    for number, x in fail_stage:
+        answer.append(number)
 
     return answer
 
-print(solution([1,3,2,5,4], 9))
+print(solution(5, [2, 1, 2, 6, 2, 4, 3, 3]))
+print(solution(4, [4, 4, 4, 4, 4]))
--- Programmers/LevelTest02.py
+++ Programmers/LevelTest02.py
@@ -1,22 +1,60 @@
-def solution(n,a,b):
-    if a > b:
-        a, b = b, a
+def solution(m, musicinfos):
+    answer = []
+    mm = []
+    now_m = ''
+    for i in range(len(m)):
+        if now_m and m[i] != '#':
+            mm.append(now_m)
+            now_m = m[i]
+            continue
+        if now_m and m[i] == '#':
+            now_m += '#'
+            mm.append(now_m)
+            now_m = ''
+            continue
+        now_m += m[i]
+    if now_m:
+        mm.append(now_m)
 
-    def next_round(k):
-        if k % 2:
-            k //= 2
-            k += 1
+    cnt = 0
+    for musicinfo in musicinfos:
+        start, end, name, e = musicinfo.split(',')
+        start_hour, start_min = int(start[:2]), int(start[3:])
+        end_hour, end_min = int(end[:2]), int(end[3:])
+        play_time = (end_hour - start_hour) * 60 + (end_min - start_min)
+        em = []
+        now_e = ''
+        for i in range(len(e)):
+            if now_e and e[i] != '#':
+                em.append(now_e)
+                now_e = e[i]
+                continue
+            if now_e and e[i] == '#':
+                now_e += '#'
+                em.append(now_e)
+                now_e = ''
+                continue
+            now_e += e[i]
+        if now_e:
+            em.append(now_e)
+        if play_time <= len(em):
+            em = em[:play_time]
         else:
-            k //= 2
-        return k
-    answer = 1
-    while True:
-        if b - a == 1 and a % 2 and not b % 2:
-            break
-        a = next_round(a)
-        b = next_round(b)
-        answer += 1
+            em = em * (play_time//len(em)) + em[:play_time%len(em)]
+        check_em = ''
+        for i in range(len(em)-len(mm)+1):
+            for j in range(i, i+len(mm)):
+                check_em += em[j]
+            print(check_em)
+            if check_em == m:
+                answer.append((cnt, play_time, name))
+                break
+            check_em = ''
+        cnt += 1
+    if not answer:
+        return '(None)'
+    answer.sort(key=lambda x:[-x[1], x[0]])
+    print(answer)
+    return answer[0][2]
 
-    return answer
-
-print(solution(8, 4 ,7))
+print(solution('CC#BCC#BCC#BCC#B', ['03:00,03:30,FOO,CC#B', '04:00,04:08,BAR,CC#BCC#BCC#B']))

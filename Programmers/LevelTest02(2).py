510652e6e2c63b0da46fd28a052e528ac5e49d28
yeonjae <duseh73@gmail.com>
2021-02-18 16:48:15 +0900

    20210218

--- Programmers/LevelTest02(2).py
+++ Programmers/LevelTest02(2).py
@@ -1,27 +1,50 @@
-def solution(cacheSize, cities):
-    if not cacheSize:
-        return len(cities) * 5
+from collections import deque
+
+def solution(bridge_length, weight, truck_weights):
     answer = 0
-    cash = []
-    for i in range(len(cities)):
-        cities[i] = cities[i].lower()
-    for city in cities:
-        if not cash:
-            cash.append(city)
-            answer += 5
+    bridge = deque([0] * bridge_length)
+
+    while truck_weights:
+        if sum(bridge)+truck_weights[0] <= weight:
+            if bridge[-1] != 0:
+                bridge.rotate(-1)
+            bridge[-1] = truck_weights[0]
+            truck_weights.pop(0)
+            answer += 1
         else:
-            if city not in cash:
-                if len(cash) < cacheSize:
-                    cash.append(city)
-                else:
-                    cash.pop(0)
-                    cash.append(city)
-                answer += 5
-            else:
-                cash.pop(cash.index(city))
-                cash.append(city)
+            while True:
+                if bridge[0] != 0:
+                    break
+                bridge.rotate(-1)
                 answer += 1
+            if bridge[0] != 0:
+                bridge[0] = 0
+                bridge.rotate(-1)
+                if sum(bridge)+truck_weights[0] <= weight:
+                    if bridge[-1] != 0:
+                        bridge.rotate(-1)
+                    bridge[-1] = truck_weights[0]
+                    truck_weights.pop(0)
+                answer += 1
+
+    while True:
+        if sum(bridge) == 0:
+            break
+        if bridge[0] != 0:
+            bridge[0] = 0
+
+        bridge.rotate(-1)
+        answer += 1
 
     return answer
 
-print(solution(0, ['Jeju', 'Pangyo', 'Seoul', 'NewYork', 'LA', 'Jeju', 'Pangyo', 'Seoul', 'NewYork', 'LA']))
+print(solution(1, 2, [1, 1, 1]))
+print(solution(1,1, [1,1,1]))
+print(solution(4, 2, [1,1,1,1]))
+print(solution(3, 3, [1, 1, 1]))
+print(solution(3, 1, [1,1,1]))
+print(solution(5, 5, [1,1,1,1,1,2,2]))
+print(solution(7, 7, [1,1,1,1,1,3,3]))
+print(solution(5, 5, [1,1,1,1,1,2,2,2,2]))
+print(solution(5, 5, [2,2,2,2,1,1,1,1,1]))
+
--- Programmers/LevelTest02.py
+++ Programmers/LevelTest02.py
@@ -1,60 +1,17 @@
-def solution(m, musicinfos):
+def solution(prices):
     answer = []
-    mm = []
-    now_m = ''
-    for i in range(len(m)):
-        if now_m and m[i] != '#':
-            mm.append(now_m)
-            now_m = m[i]
-            continue
-        if now_m and m[i] == '#':
-            now_m += '#'
-            mm.append(now_m)
-            now_m = ''
-            continue
-        now_m += m[i]
-    if now_m:
-        mm.append(now_m)
-
-    cnt = 0
-    for musicinfo in musicinfos:
-        start, end, name, e = musicinfo.split(',')
-        start_hour, start_min = int(start[:2]), int(start[3:])
-        end_hour, end_min = int(end[:2]), int(end[3:])
-        play_time = (end_hour - start_hour) * 60 + (end_min - start_min)
-        em = []
-        now_e = ''
-        for i in range(len(e)):
-            if now_e and e[i] != '#':
-                em.append(now_e)
-                now_e = e[i]
-                continue
-            if now_e and e[i] == '#':
-                now_e += '#'
-                em.append(now_e)
-                now_e = ''
-                continue
-            now_e += e[i]
-        if now_e:
-            em.append(now_e)
-        if play_time <= len(em):
-            em = em[:play_time]
-        else:
-            em = em * (play_time//len(em)) + em[:play_time%len(em)]
-        check_em = ''
-        for i in range(len(em)-len(mm)+1):
-            for j in range(i, i+len(mm)):
-                check_em += em[j]
-            print(check_em)
-            if check_em == m:
-                answer.append((cnt, play_time, name))
+    for i in range(len(prices)-1):
+        time = 0
+        flag = True
+        for j in range(i+1, len(prices)):
+            if prices[i] > prices[j]:
+                time += 1
+                answer.append(time)
+                flag = False
                 break
-            check_em = ''
-        cnt += 1
-    if not answer:
-        return '(None)'
-    answer.sort(key=lambda x:[-x[1], x[0]])
-    print(answer)
-    return answer[0][2]
+            time += 1
+        if flag:
+            answer.append(time)
+    return answer + [0]
 
-print(solution('CC#BCC#BCC#BCC#B', ['03:00,03:30,FOO,CC#B', '04:00,04:08,BAR,CC#BCC#BCC#B']))
+print(solution([1, 2, 3, 2, 3]))

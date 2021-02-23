26b287924cdee5a75ab63cadde6ce9f4f5791ace
yeonjae <duseh73@gmail.com>
2021-02-23 15:34:42 +0900

    20210223

--- Programmers/LevelTest02.py
+++ Programmers/LevelTest02.py
@@ -1,17 +1,17 @@
-def solution(prices):
-    answer = []
-    for i in range(len(prices)-1):
-        time = 0
-        flag = True
-        for j in range(i+1, len(prices)):
-            if prices[i] > prices[j]:
-                time += 1
-                answer.append(time)
-                flag = False
-                break
-            time += 1
-        if flag:
-            answer.append(time)
-    return answer + [0]
+def solution(s):
+    if s[0] == ')':
+        return False
+    stack = []
+    for i in range(len(s)):
+        if s[i] == ')' and stack:
+            stack.pop()
+        else:
+            stack.append(s[i])
+    if stack:
+        return False
+    return True
 
-print(solution([1, 2, 3, 2, 3]))
+print(solution('()()'))
+print(solution('(())()'))
+print(solution(')()('))
+print(solution('(()('))

85e3d86a0aa59597744f2d841954e0e80de8d242
yeonjae <duseh73@gmail.com>
2021-02-25 17:04:19 +0900

    20210225

--- Programmers/LevelTest02(2).py
+++ Programmers/LevelTest02(2).py
@@ -1,42 +1,23 @@
-language_dic = {
-    'cpp': 0,
-    'java': 0,
-    'python': 0
-}
+def transN(n):
+    if n == 1:
+        return 1
+    answer = ''
+    while n // 2 >= 1:
+        remain = n % 2
+        n = n // 2
+        answer = str(remain) + answer
+        if n < 2:
+            answer = str(n) + answer
+    return answer.count('1')
 
-field_dic = {
-    'backend': 0,
-    'frontend': 0,
-}
+def solution(n):
+    check = transN(n)
+    while True:
+        n += 1
+        if transN(n) == check:
+            return n
 
-level_dic = {
-    'junior': 0,
-    'senior': 0
-}
 
-food_dic = {
-    'chicken': 0,
-    'pizza': 0
-}
-def solution(info, query):
-    answer = []
-    query_ls = []
-    for i in range(len(info)):
-        info_ls = info[i].split()
-        language_dic[info_ls[0]] += 1
-        field_dic[info_ls[0]] += 1
-        level_dic[info_ls[0]] += 1
-        food_dic[info_ls[0]] += 1
-    for i in range(len(query)):
-        mid_query = query[i].split(' and ')
-        remain = mid_query[3].split()
-        mid_query = mid_query[:3]
-        for j in range(2):
-            mid_query.append(remain[j])
-        query_ls.append(mid_query)
-    print(query_ls)
-    return answer
-
-print(solution(["java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"]
-               , ["java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"]))
+print(solution(78))
+print(solution(15))
 
--- Programmers/LevelTest02.py
+++ Programmers/LevelTest02.py
@@ -1,16 +1,18 @@
-def solution(number, k):
-    answer = ''
-    stack = [int(number[0])]
-    for i in range(1, len(number)):
-        while stack and stack[-1] < int(number[i]) and k > 0:
-            stack.pop()
-            k -= 1
-        stack.append(int(number[i]))
-    stack = stack[:(len(number)-k)]
-    for n in stack:
-        answer += str(n)
+from itertools import combinations
+def is_prime(k):
+    for i in range(2, k):
+        if not k % i:
+            return False
+    return True
+
+def solution(nums):
+    answer = 0
+    for num in combinations(nums, 3):
+        if is_prime(sum(num)):
+            answer += 1
+
     return answer
 
-print(solution('1924', 2))
-print(solution('1231234', 3))
-print(solution('4177252841', 4))
+print(solution([1, 2, 3, 4]))
+print(solution([1, 2, 7, 6, 4]))
+

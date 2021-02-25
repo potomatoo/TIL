546793bec36b636aad43aa6489e3904f3aef3488
yeonjae <duseh73@gmail.com>
2021-02-25 22:00:39 +0900

    20210225

--- Programmers/LevelTest03(2).py
+++ Programmers/LevelTest03(2).py
@@ -1,29 +1,19 @@
-from copy import deepcopy
-
-def solution(tickets):
-    def dfs(now, k):
-        if k == len(tickets)-1:
-            c_order = deepcopy(order)
-            answer.append(c_order)
-        for i in range(len(tickets)):
-            if visit[i]:
-                continue
-            if now == tickets[i][0]:
-                visit[i] = 1
-                order.append(tickets[i][1])
-                dfs(tickets[i][1], k + 1,)
-                visit[i] = 0
-                order.pop()
-    answer = []
-    for i in range(len(tickets)):
-        if tickets[i][0] == 'ICN':
-            visit = [0] * len(tickets)
-            visit[i] = 1
-            order = ['ICN', tickets[i][1]]
-            dfs(tickets[i][1], 0)
-    answer.sort()
-    return answer[0]
+def solution(n, times):
+    answer = 0
+    left = 1
+    right = max(times) * n
+    while left <= right:
+        mid = (left + right) // 2
+        pass_people = 0
+        for time in times:
+            pass_people += (mid // time)
+        if pass_people >= n:
+            answer = mid
+            right = mid - 1
+        else:
+            left = mid + 1
+    return answer
 
 
-print(solution([['ICN', 'JFK'], ['HND', 'IAD'], ['JFK', 'HND']]))
-print(solution([['ICN', 'SFO'], ['ICN', 'ATL'], ['SFO', 'ATL'], ['ATL', 'ICN'], ['ATL','SFO']]))
+print(solution(6, [7, 10]))
+
--- Programmers/kakao_줄 서는 방법.py
+++ Programmers/kakao_줄 서는 방법.py
@@ -2,7 +2,7 @@ import math
 def solution(n, k):
     answer = [num for num in range(1,n+1)]
     answer_list = []
-    while n > 0 :
+    while n > 0:
         n -= 1
         share, remainder = divmod(k,math.factorial(n))
         if not remainder: share -= 1

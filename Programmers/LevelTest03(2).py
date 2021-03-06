b932195d652f18e577d04b6ee423436668de9525
yeonjae <duseh73@gmail.com>
2021-03-06 17:40:32 +0900

    20210306

--- Programmers/LevelTest03(2).py
+++ Programmers/LevelTest03(2).py
@@ -1,30 +1,42 @@
-def solution(genres, plays):
-    answer = []
-    time_dic = dict()
-    order_dic = dict()
-    for i in range(len(genres)):
-        if genres[i] not in time_dic:
-            time_dic[genres[i]] = plays[i]
-            order_dic[genres[i]] = [(i, plays[i])]
-        else:
-            time_dic[genres[i]] += plays[i]
-            order_dic[genres[i]].append((i, plays[i]))
+import heapq
 
-    time = []
-    for key, value in time_dic.items():
-        time.append((key, value))
-    time.sort(key=lambda x: -x[1])
-    for genre, t in time:
-        cnt = 0
-        order_dic[genre].sort(key=lambda x: [-x[1], x[0]])
-        if len(order_dic[genre]) > 2:
-            while cnt != 2:
-                answer.append(order_dic[genre][cnt][0])
-                cnt += 1
+def solution(n, s, a, b, fares):
+    answer = 0xffffff
+    road = [[] for _ in range(n+1)]
+    for start, end, cost in fares:
+        road[start].append((end, cost))
+        road[end].append((start, cost))
+
+    def djikstra(start, a, b):
+        INF = 1e9
+        dist = [INF] * (n + 1)
+        dist[start] = 0
+        Q = []
+        heapq.heappush(Q, (start, 0))
+        while Q:
+            now, now_cost = heapq.heappop(Q)
+            if dist[now] < now_cost:
+                continue
+            for end, cost in road[now]:
+                new_cost = now_cost + cost
+                if new_cost < dist[end]:
+                    dist[end] = new_cost
+                    heapq.heappush(Q, (end, new_cost))
+        if a == 0 and b == 0:
+            return dist
         else:
-            for kk in order_dic[genre]:
-                answer.append(kk[0])
+            return dist[a] + dist[b]
+
+    mid_dist = djikstra(s, 0, 0)
+
+    for i in range(1, n+1):
+        mid_answer = mid_dist[i]
+        mid_answer += djikstra(i, a, b)
+        answer = min(answer, mid_answer)
+
     return answer
 
-print(solution(["classic", "pop", "classic", "classic", "pop"], [500, 600, 150, 800, 2500]))
+print(solution(6, 4, 6, 2, [[4, 1, 10], [3, 5, 24], [5, 6, 2], [3, 1, 41], [5, 1, 24], [4, 6, 50], [2, 4, 66], [2, 3, 22], [1, 6, 25]]))
+print(solution(7, 3, 4, 1, [[5, 7, 9], [4, 6, 4], [3, 6, 1], [3, 2, 3], [2, 1, 6]]))
+print(solution(6, 4, 5, 6, [[2,6,6], [6,3,7], [4,6,7], [6,5,11], [2,5,12], [5,3,20], [2,4,8], [4,3,9]]))
 
